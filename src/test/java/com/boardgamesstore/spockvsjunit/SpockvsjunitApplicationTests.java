6c84126da781d1638f675dfc960328892f56a443
zarkob <zarkob@users.noreply.github.com>
2023-11-04 10:04:22 +0100

    init

---
+++ src/test/java/com/boardgamesstore/spockvsjunit/SpockvsjunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardgamesstore.spockvsjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpockvsjunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
